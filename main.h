682c5866b0131836d48f89359f07e947c5f448b1
NielsvdL <nielsvanderlaan@hotmail.com>
2021-01-27 13:59:18 +0100

    Debugging three-stage models

---
+++ main.h
@@ -0,0 +1,15 @@
+#ifndef MSP_MAIN_H
+#define MSP_MAIN_H
+
+#include <iostream>
+
+#include "gurobi_c++.h"
+
+#include "tree/tree.h"
+#include "data/instances.h"
+
+using namespace std;
+using namespace arma;
+
+
+#endif //MSP_MAIN_H
